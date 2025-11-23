4dc46046c70183557b757466d957c3a81763e987
Kotaro <151450999+kotaro1337@users.noreply.github.com>
2025-11-24 00:51:50 +0800

    Add files via upload

---
+++ anti-tanga.js
@@ -0,0 +1,5 @@
+The page could not be found
+
+NOT_FOUND
+
+fra1::mf7mw-1737464879116-86fe33503eb0
---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1167776183392403529?theme=true&bg=00000000&idleMessage=&showDisplayName=true&animated=true&idleMessage=offline";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
